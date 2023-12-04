82d43b05a4a3ff1dea90def830f8dd4c9daad94f
Marc0w <argentina.marco2022@gmail.com>
2023-12-04 15:14:53 +0000

    Update Nav bar Registar

--- Interface/registar.php
+++ Interface/registar.php
@@ -1,138 +1,3 @@
-<!-- <?php //include("../DataAccessLayer/conectionBD.php")
-        ?>
-<!DOCTYPE html>
-<html lang="en">
-   
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Registration Form</title>
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
- <link rel="stylesheet" href="css/style.css">
-</head>
-    <body>
-    <?php
-    // if(isset($_POST['btSubmit'])){
-    // $flag=false;
-    // $flag_email=false;
-    // $flag_password=false;
-
-
-    // $first_name=$_POST['first_name'];
-    // $last_name=$_POST['last_name'];
-    // $user_name=$_POST['user_name'];
-    // $email=$_POST['email'];
-    // $password=$_POST['password'];
-    // $confirm_password=$_POST['confirmpassword'];
-    // /* Verificar se o login já existe */
-    // $query="select email from users ";
-    // $result=mysqli_query($conexao,$query);
-    // while($registo=mysqli_fetch_assoc($result)){
-    //     $emailBD=$registo['email'];
-    //     if($emailBD==$email){
-    //         $flag=true;
-    //         $flag_email=true;
-    //         }		
-    //     }
-    // /* Validações */		
-    // if ($password!=$confirm_password || $password=="") {$flag=true; $flag_password=true;}
-
-
-    // /* Existiu um erro */
-    // if($flag_password==true){ 
-    ?>
-        <script> alert("Senha diferente! ")</script><?php
-                                                    ?>
-            <section>
-    <div class="form-container">
-        <form id="registrationForm" action="" method="post">
-            <div class="form-group">
-                <label for="first_name">First name:</label>
-                <input type="text" id="first_name" name="first_name">
-            </div>
-            <div class="form-group">
-                <label for="last_name">Last name:</label>
-                <input type="text" id="last_name" name="last_name">
-            </div>
-            <div class="form-group">
-                <label for="user_name">user_name: <span class="required">*</span></label>
-                <input type="text" id="user_name" name="user_name" required>
-            </div>
-            <div class="form-group">
-                <label for="email">Email: <span class="required">*</span></label>
-                <input type="email" id="email" name="email" required>
-            </div>
-            <div class="form-group">
-                <label for="password">Password: <span class="required">*</span></label>
-                <input type="password" id="password" name="password" required>
-            </div>
-            <div class="form-group">
-                <label for="confirm-password">Confirm Password:</label>
-                <input type="password" id="confirmpassword" name="confirmpassword">
-            </div>
-            <input type="submit" class="" name="btSubmit" id="btSubmit" value="Submit" class="primary" />
-            <div id="validationErrors"></div>
-        </form>
-    </div>
-        </section>
-    <?php //} 
-    //     else {
-    //         $password=password_hash($password, PASSWORD_DEFAULT);
-    //         $insert="INSERT INTO users  
-    // (user_types_id,first_name, last_name,user_name,email,password) VALUES (1,'".$first_name."','".$last_name."','".$user_name."','".$email."','".$password."')";
-    // $result=mysqli_query($conexao,$insert);
-
-    // if($result==1){	
-    // echo"<p>Parabéns $first_name! Realizou o seu registo com sucesso.</p>";
-    // } 
-
-    // else {
-    // echo "<p>Dados não inseridos!</p>";
-    ?><br><br>
-// <a href="index.php" class="form-submit">Voltar ao Menu Principal</a><?php
-                                                                        // }
-                                                                        // }
-                                                                        // }else {
-                                                                        ?>
-    <div class="form-container">
-        <form id="registrationForm" action="" method="post">
-            <div class="form-group">
-                <label for="first_name">First name:</label>
-                <input type="text" id="first_name" name="first_name">
-            </div>
-            <div class="form-group">
-                <label for="last_name">Last name:</label>
-                <input type="text" id="last_name" name="last_name">
-            </div>
-            <div class="form-group">
-                <label for="user_name">user_name: <span class="required">*</span></label>
-                <input type="text" id="user_name" name="user_name" required>
-            </div>
-            <div class="form-group">
-                <label for="email">Email: <span class="required">*</span></label>
-                <input type="email" id="email" name="email" required>
-            </div>
-            <div class="form-group">
-                <label for="password">Password: <span class="required">*</span></label>
-                <input type="password" id="password" name="password" required>
-            </div>
-            <div class="form-group">
-                <label for="confirm-password">Confirm Password:</label>
-                <input type="password" id="confirmpassword" name="confirmpassword">
-            </div>
-            <div class="form-group">
-            
-            <input type="submit" class="" name="btSubmit" id="btSubmit" value="Submit" class="primary" />
-            </div>
-            <div id="validationErrors"></div>
-        </form>
-    </div>  
- <?php //} 
-    ?> -->
-<!-- <script src="script.js"></script>
-    </body>
-</html> -->
-
 <?php include("../Controller/registUserController.php"); ?>
 
 <?php
@@ -166,15 +31,15 @@ ob_start(); // Inicia o buffer de saída
                 <input type="text" id="last_name" name="last_name">
             </div>
             <div class="form-group">
-                <label for="user_name">Username: <span class="required">*</span></label>
+                <label for="user_name">Username: <span class="required"></span></label>
                 <input type="text" id="user_name" name="user_name" required>
             </div>
             <div class="form-group">
-                <label for="email">Email: <span class="required">*</span></label>
+                <label for="email">Email: <span class="required"></span></label>
                 <input type="email" id="email" name="email" required>
             </div>
             <div class="form-group">
-                <label for="password">Password: <span class="required">*</span></label>
+                <label for="password">Password: <span class="required"></span></label>
                 <input type="password" id="password" name="password" required>
             </div>
             <div class="form-group">
@@ -193,5 +58,5 @@ ob_start(); // Inicia o buffer de saída
 <?php
 $content = ob_get_clean(); // Obtém o conteúdo do buffer e limpa o buffer
 
-include('pageDefault.php'); // Inclui o arquivo do template com o conteúdo especificado
+include('pageDefaultMenu.php'); // Inclui o arquivo do template com o conteúdo especificado
 ?>
