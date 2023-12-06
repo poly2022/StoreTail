9f4a071943fdf51cb8fdf16ea05e9c2e185eadb5
poly2022 <pollybraga.17@hotmail.com>
2023-12-06 19:34:13 +0000

    home

--- Interface/index.php
+++ Interface/index.php
@@ -9,15 +9,12 @@ ob_start(); // Inicia o buffer de saída
 <div class="content">
 
 <div class="container-fluid d-flex justify-content-center align-items-center bg-body-tertiary flex-column">
-      <h2 style="color: #E95A0C;" class="text-center mt-4">New Books</h2>
-      <div class="container bg-white py-4 mb-4" style="max-height: 80vh; overflow-y: auto;">
         <div class="row row-cols-1 row-cols-md-4 g-4">
           <?php include("../Interface/book.php") ?>
           </div>
         </div>
       </div>
     </div>
-
 </div>
 <?php
 $content = ob_get_clean(); // Obtém o conteúdo do buffer e limpa o buffer
