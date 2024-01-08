691f9a35cdb2475528415bb9db2245d8985e5734
poly2022 <pollybraga.17@hotmail.com>
2024-01-08 20:44:44 +0000

    comentários

--- Interface/about.php
+++ Interface/about.php
@@ -1,10 +1,13 @@
 <?php
+    // Inicia o buffer de saída
     ob_start();
 ?>
 <div class="content">
     <div class="text-center mb-5">
-        <img src="../Interface/img/storytail-logo-02.png" alt="Logo da Empresa" style="width: 200px; border-radius: 4px" >
+        <!-- Logo da Empresa -->
+        <img src="../Interface/img/storytail-logo-02.png" alt="Logo da Empresa" style="width: 200px; border-radius: 4px">
     </div>
+    <!-- Títulos e parágrafos -->
     <h2>Storytail Presentation</h2>
     <h5>Embark on a Magical Reading Adventure</h5>
     <p>At Storytail, we believe that every book is a ticket to a thrilling adventure. 
@@ -12,6 +15,7 @@
     introduce them to fascinating characters, and ignite their love for reading. </p>
     <br></br>
 
+    <!-- Informações de contato -->
     <h2>Contact</h2>
     <p>If you have any questions about these terms, please contact us.</p>
     <p class="mb-0">info@storytail.pt</p>
@@ -19,7 +23,9 @@
     <p class="mb-0">Rua Nova da Raposa n.º 164, Vila Nova de Gaia</p>
 </div>
 <?php
+    // Captura o conteúdo do buffer de saída e limpa o buffer
     $content = ob_get_clean();
 
+    // Inclui o conteúdo gerado em 'pageDefault.php'
     include('pageDefault.php');
 ?>
--- Interface/contact.php
+++ Interface/contact.php
@@ -1,26 +1,29 @@
 <?php
+// Verifica se o formulário foi submetido via método POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Captura os dados do formulário
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $mensagem = $_POST['mensagem'];
 
+    // Configuração do e-mail de destino e conteúdo da mensagem
     $to = 'seu-email@exemplo.com'; // Substitua pelo seu endereço de e-mail
     $assunto = 'Nova mensagem do formulário de contato';
     $corpo_mensagem = "Nome: $nome\n";
     $corpo_mensagem .= "Email: $email\n";
     $corpo_mensagem .= "Mensagem:\n\n$mensagem";
 
-    // Enviar e-mail
+    // Envia o e-mail
     mail($to, $assunto, $corpo_mensagem);
 
+    // Redireciona de volta para 'index.php' e exibe um alerta de sucesso
     echo "<script> 
-                    window.location.href='./index.php';
-                     alert(' SUCESSO - Sua mensagem foi enviado com sucesso.'); 
-                 </script>";
+            window.location.href='./index.php';
+            alert('SUCESSO - Sua mensagem foi enviada com sucesso.'); 
+          </script>";
     exit;
 }
 ?>
-
 
 <style>
     /* Estilos CSS */
@@ -34,6 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <?php
 
+// Inicia o buffer de saída
 ob_start();
 ?>
 <div class="content">
@@ -41,6 +45,7 @@ ob_start();
         <img src="../Interface/img/storytail-logo-02.png" alt="Logo da Empresa" style="width: 200px; border-radius: 4px">
     </div>
     <h3>Contact us</h3>
+    <!-- Formulário de contato -->
     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
         <label for="nome">Name:</label>
         <input type="text" id="nome" name="nome" required><br><br>
@@ -48,14 +53,16 @@ ob_start();
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required><br><br>
 
-        <label for="mensagem">Menssage:</label><br>
+        <label for="mensagem">Message:</label><br>
         <textarea id="mensagem" name="mensagem" rows="4" required></textarea><br><br>
 
         <input type="submit" value="Enviar">
     </form>
 </div>
 <?php
+// Captura o conteúdo do buffer de saída e limpa o buffer
 $content = ob_get_clean();
 
+// Inclui o conteúdo gerado em 'pageDefault.php'
 include('pageDefault.php');
 ?>
